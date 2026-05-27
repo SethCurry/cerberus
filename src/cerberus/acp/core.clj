d29721c993ec2d2cdc75550ad971964c3265b7d2
Seth Curry <seth@scurry.io>
2026-05-27 17:35:27 -0400

    Working on main agent fn

--- src/cerberus/acp/core.clj
+++ src/cerberus/acp/core.clj
@@ -1,10 +1,11 @@
 (ns cerberus.acp.core
-  (:require [jsonista.core :as json]))
+  (:require [jsonista.core :as json]
+    [cerberus.acp.schemas.api :as api]))
 
-(defn read-input-message []
+(defn read-input-message [buf]
   (let [got-value (json/read-value
     json/keyword-keys-object-mapper
-    (java.io.BufferedReader. *in*))]
+    buf)]
   (t/log! {:level :debug
     :msg "read message from stding"
     :data {
@@ -14,3 +15,12 @@
 
 (defn write-output-message [msg]
   (json/write-value json/keyword-keys-object-mapper *out*))
+
+(defn run-agent []
+  (let [read-buf (java.io.BufferedReader. *in*)]
+    (while true
+      (let [got-message (read-input-message read-buf)
+        message-schema (api/get-message-schema got-message)
+        is-valid (malli/validate message-schema got-message)]
+      (when (not is-valid)
+        (throw (ex-info "got invalid message" {:validation is-valid :message got-message})))))))
